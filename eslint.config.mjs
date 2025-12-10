a4dfd62280a56fbd2c2f12a004b36b32faec150f
Wajahat Islam Gul <aliwajahat44@yahoo.com>
2025-12-10 11:22:55 +0500

    chore: Setup lint and format

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import tailwindcss from 'eslint-plugin-tailwindcss'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+const customIgnores = globalIgnores([
+  '.next/**',
+  'out/**',
+  'build/**',
+  'dist/**',
+  'coverage/**',
+  'node_modules/**',
+  '*.log',
+])
+
+export default defineConfig([
+  ...nextVitals,
+  customIgnores,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      tailwindcss,
+    },
+    rules: {
+      'tailwindcss/classnames-order': 'error',
+      'react/no-unescaped-entities': 'off',
+    },
+  },
+  eslintConfigPrettier,
+])
